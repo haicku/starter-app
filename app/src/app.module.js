fdf66c27b1096c2ae9d9ddce4f29456b1df32b9d
jgpATs2w <jgp@sens2web.es>
2016-05-16 23:18:07 +0200

    auth working, defaultHome added

--- app/src/app.module.js
+++ app/src/app.module.js
@@ -16,22 +16,37 @@
     .config(configure)
     .run(runBlock);
 
-  configure.$inject = ['$routeProvider'];
+  configure.$inject = ['$routeProvider', '$locationProvider'];
 
-  function configure($routeProvider) {
+  function configure($routeProvider, $locationProvider) {
+
+    //custom
+    $routeProvider.when('/', {
+      templateUrl: 'app/src/core/defaultHome.html'
+    });
+
+    //core
+    $locationProvider.html5Mode(true);
+    $routeProvider.when('/notfound', {
+      templateUrl: 'app/src/core/notfound.html'
+    });
+
     $routeProvider.otherwise({
-      redirectTo: '/'
+      redirectTo: '/notfound'
     });
   }
 
   runBlock.$inject = ['$rootScope', '$location'];
 
-  function runBlock($rootScope, $location) {
+  function runBlock($rootScope, $location ) {
+
     $rootScope.$on('$routeChangeError', function(event, next, previous, error) {
-      if (error === "AUTH_REQUIRED") {
+      if (error === "AUTH_REQUIRED") { console.error("app.runBlock# AUTH_REQUIRED");
         $location.path('/');
       }
     });
+
+
   }
 
 })();
